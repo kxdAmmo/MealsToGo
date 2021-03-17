da3ecc805564d93ef2e2419dc853882cc745e624
kxdAmmo <fungaip163@outlook.com>
2021-03-17 19:11:34 +0200

    Added react-native-svg-transformer

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -1,37 +1,67 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+// import { SvgXml } from "react-native-svg";
+
+import { Crown } from "../../../../assets/crown.svg";
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colours.ui.primary};
-  font-size: ${(props) => props.theme.sizes[1]};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
 `;
+// background-color: ${(props) => props.theme.colours.bg.primary};
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colours.bg.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colours.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
     icon,
-    photos = ["https://picsum.photos/700"],
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {console.log(<Crown />)}
+          {/* {ratingArray.map(() => (
+          ))} */}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
